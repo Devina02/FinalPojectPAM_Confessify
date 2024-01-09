1ff5b73b24cc856c3bbfc0a8234e0023b12a0594
saraz <bagas.saras.ft21@mail.umy.ac.id>
2024-01-09 19:23:41 +0700

    Membuat interface ConfessService.kt

--- app/src/main/java/com/example/finalprojectpam_confessify/service_api/ConfessService.kt
+++ app/src/main/java/com/example/finalprojectpam_confessify/service_api/ConfessService.kt
@@ -1,5 +1,24 @@
 package com.example.finalprojectpam_confessify.service_api
 
-interface ConfessService {
+import com.example.finalprojectpam_confessify.data.ConfessData
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
+interface ConfessService {
+    @Headers(
+        "Accept: application/json"
+    )
+
+    @GET("/Confess")
+    suspend fun getConfess(): List<ConfessData>
+
+    @POST("Confess")
+    suspend fun insertConfess(@Body Confess: ConfessData)
+
+    @PUT("Confess/{id}")
+    suspend fun updateConfess(@Query("id") @Body Confess: ConfessData)
 }
